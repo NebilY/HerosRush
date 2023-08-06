598450c165eb3b434cc25cb9dbe83b2b6c0b63d7
NebilY <nebil.yousuf@mnsu.edu>
2023-08-06 11:34:34 -0500

    HerosRush

---
+++ Assets/Enemyweapon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Enemyweapon : MonoBehaviour
+{
+    public int attackDamage = 20;
+    public int enragedAttackDamage = 40;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+
+    public AudioClip impactSound; // Reference to AudioClip
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            colInfo.GetComponent<PlayerHealths>().TakeDamage(attackDamage);
+            AudioSource.PlayClipAtPoint(impactSound, transform.position); // Play sound effect when weapon collides with target
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
+}
